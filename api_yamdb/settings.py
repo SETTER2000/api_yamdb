0c7f4b2a451352d733625898e41f155d2a72d978
SETTER <lphp@mail.ru>
2021-08-31 14:01:02 +0300

    README

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -99,8 +99,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_URLS_REGEX = r"^/api/.*$"
 
